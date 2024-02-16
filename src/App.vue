edec3a0724375dd8f9db28718eb89ade92bcc33d
programming473 <merser.max@mail.ru>
2024-02-16 18:26:55 +0300

    feat: change folder and add fonts

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-  
+	<div>111</div>
 </template>
 
 <style>
-
+@import './assets/fonts/Roboto/Roboto.css';
+@import './assets/fonts/Gothic-A1/Gothic-A1.css';
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/index.css'
 
 const app = createApp(App)
 
